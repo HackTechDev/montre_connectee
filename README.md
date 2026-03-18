ac9a861f996a66e34da49707e40337049dc4b968
Nekrofage <lesanglierdesardennes@gmail.com>
2026-03-18 21:01:52 +0100

    [Version] 0.0.1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Montre connectée
+
+### Matériels :  
+
+- Carte NodeMCU ESP32 (WiFi - Bluetooth - micro-USB) :  
+https://www.gotronic.fr/art-module-nodemcu-esp32-28407.htm  
+
+- Module d'affichage couleur OLED de 0,49 pouce, résolution d'interface IIC 128 * 32 Module à 4 broches (blanche) :    
+https://www.amazon.fr/dp/B09FQ42BG9  
+
+
+### Librairies :   
+- Adafruit GFX library v1.12.5  
+- Adafruit SSD1306 v2.5.16  
+
+
+### Configuration Arduino :
+- NodeMCU 1.O (ESP-12E Module)  
+
+
---
+++ montre.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32// OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  
+  delay(2000);
+  
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.setCursor(40, 20);
+  display.println("HACKLAB");
+  display.display(); 
+
+  delay(5000);
+  display.clearDisplay();
+
+  display.setCursor(40, 20);
+  display.println("ILARD");
+  display.display();
+
+  delay(5000);
+  display.clearDisplay();
+
+  display.setCursor(40, 20);
+  display.println("SAM");
+  display.display();
+
+  delay(5000);
+  display.clearDisplay();
+
+  display.setCursor(40, 20);
+  display.println("NEKROS");
+  display.display();
+
+
+  delay(5000);
+  display.clearDisplay();
+
+  display.drawCircle(64, 20, 5, SSD1306_WHITE);
+  display.display();
+
+  delay(5000);
+  
+  display.clearDisplay();
+
+  display.drawRect(32, 16, 64, 16, SSD1306_WHITE);
+
+  display.drawLine(32, 24, 128, 24, SSD1306_WHITE);
+  display.drawLine(64, 16, 64, 48, SSD1306_WHITE);
+
+  display.display();
+
+
+}
+
+void loop() {
+  
+}
